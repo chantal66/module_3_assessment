3450f360472dc3098827282fa05ae5dfe7aafae8
Chantal Justamond <cjustamm66@me.com>
2017-09-13 09:10:39 -0600

    test and figaro with key

---
+++ spec/requests/api_endpoints_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe 'Items API Endpoints' do
+  context 'Get api/v1/items' do
+    it 'sends all items' do
+      items = create_list(:item, 3)
+
+      get '/api/v1/items'
+
+      items = JSON.parse(response.body)
+      item = items.first
+
+      expect(response).to be_success
+
+      expect(response).to have_http_status(200)
+      expect(items.count).to eq(3)
+      expect(item).to have_key 'id'
+      expect(item).to have_key 'name'
+      expect(item).to have_key 'description'
+      expect(item).to have_key 'image_url'
+      expect(item).not_to have_key 'created_at'
+      expect(item).not_to have_key 'updated_at'
+    end
+  end
+end
