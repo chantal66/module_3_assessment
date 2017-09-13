a21aefa236fd7e2e5b749194e386fe2fb326ccf2
Chantal Justamond <cjustamm66@me.com>
2017-09-13 09:51:40 -0600

    connection made with bestbuy

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+  def index
+    @zip = params['search']
+     @conn = Faraday.new(url: "https://api.bestbuy.com") do |faraday|
+         faraday.headers["X-API-KEY"] = ENV["best_buy_key"]
+         faraday.adapter Faraday.default_adapter
+     end
+
+       response = @conn.get("v1/stores(area(#{@zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_key']}")
+
+       results = JSON.parse(response.body, symbolize_names: true)
+
+  end
+end
